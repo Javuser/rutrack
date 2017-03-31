011a8062485e6b115d5803d945b52220e3fc40e4
Javuser <agybetov.nurbakyt@gmail.com>
2017-03-31 12:01:02 +0600

    11 esep

---
+++ task11.java
@@ -0,0 +1,21 @@
+package Anything;
+
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float x, y, z, a, b;
+        x = scanner.nextInt();
+        y = scanner.nextInt();
+        z = scanner.nextInt();
+        a = (float) (Math.sqrt(Math.abs(x - 1)) + Math.sqrt(abs(x)),3)/;
+
+
+
+    }
+}
