1a0083890136b46794d6e369f021bb9a981fa313
Javuser <agybetov.nurbakyt@gmail.com>
2017-03-31 12:06:07 +0600

    13 esep

--- Main.java
+++ Main.java
@@ -1,21 +1,17 @@
 package Anything;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float m1;
-        float m2;
-        double F;
-        float r;
-        double G;
-        m1 = scanner.nextInt();
-        m2 = scanner.nextInt();
-        r = scanner.nextInt();
-        G = (float) ((float)6.61*(Math.pow(10,-11)));
-        F = (float) ((G*m1*m2)/(r*r));
-        System.out.println(" " + F);
+        float t,l,g =10,pi;
+        pi = (float) 3.14;
+        l = scanner.nextInt();
+        t = (float)( 2*pi*Math.sqrt(l/g));
+        System.out.println(" " + t);
 
     }
+
 }
