f46c26d6c3884dbf1c2bc3935da3fb8a9f2ee0b1
Javuser <agybetov.nurbakyt@gmail.com>
2017-03-31 12:14:30 +0600

    17 esep

---
+++ task17.java
@@ -0,0 +1,19 @@
+package Anything;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float r ,R,s,s1,s2,pi;
+        r = 20;
+        pi =(float) 3.14;
+        R = scanner.nextFloat();
+        s1 = r*r*pi;
+        s = R*R*pi;
+        s2 =  s - s1;
+        System.out.println(s2);
+
+    }
+}
