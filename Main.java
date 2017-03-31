0edb2471475bacd9d68c3f73d533fcb3e173d963
Javuser <agybetov.nurbakyt@gmail.com>
2017-03-31 09:48:54 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,14 +3,14 @@ package Anything;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
+// task about cube
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        float r1,r2,r3,r;
-        r1 = scanner.nextInt();
-        r2= scanner.nextInt();
-        r3 = scanner.nextInt();
-        r = 1/r1 + 1/r2 +1/r3;
-        System.out.println(" " + r);
-    }
+        float a = scanner.nextInt(),s1,s2;
+        s1 = (float) Math.pow(a,3);
+        s2= (float) Math.pow(a,2);
+        System.out.println(s1);
+        System.out.println(s2);
 }
+}
+
