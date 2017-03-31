31f011fdffbcc2be30c62949368d5dada444ebce
Javuser <agybetov.nurbakyt@gmail.com>
2017-03-31 11:51:59 +0600

    8 esep

--- Main.java
+++ Main.java
@@ -1,20 +1,8 @@
 package Anything;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        float v1,v2,t1,t2,t,v;
-        v1 = scanner.nextInt();
-        v2 = scanner.nextInt();
-        t1 = scanner.nextInt();
-        t2 = scanner.nextInt();
-        v = v1 + v2;
-        t = (v1*t1 + v2*t2)/v;
-        System.out.println("Установленная температура равен " + t);
-        System.out.println("Объем равен " + v);
 
     }
 }
