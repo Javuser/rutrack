e3322646fec5e18535cf73f5354815c4a0142a43
Javuser <agybetov.nurbakyt@gmail.com>
2017-03-31 11:51:22 +0600

    7 esep

--- Main.java
+++ Main.java
@@ -3,14 +3,18 @@ package Anything;
 import java.util.Scanner;
 
 public class Main {
-// task about cube
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        float a = scanner.nextInt(),s1,s2;
-        s1 = (float) Math.pow(a,3);
-        s2= (float) Math.pow(a,2);
-        System.out.println(s1);
-        System.out.println(s2);
-}
-}
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float v1,v2,t1,t2,t,v;
+        v1 = scanner.nextInt();
+        v2 = scanner.nextInt();
+        t1 = scanner.nextInt();
+        t2 = scanner.nextInt();
+        v = v1 + v2;
+        t = (v1*t1 + v2*t2)/v;
+        System.out.println("Установленная температура равен " + t);
+        System.out.println("Объем равен " + v);
+
+    }
+}
