199c5123d3c9d6a63ad56d020574e9b9f2313a38
Javuser <agybetov.nurbakyt@gmail.com>
2017-03-31 11:46:32 +0600

    4 esep

---
+++ task4.java
@@ -0,0 +1,15 @@
+package Anything;
+
+import java.util.Scanner;
+
+public class Main {
+// task about arithmetic and geometric mean
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float x,y;
+        x = scanner.nextInt();
+        y = scanner.nextInt();
+        System.out.println(" " + (x+y)/2);
+        System.out.println(" " + Math.sqrt(x*y));
+    }
+}
