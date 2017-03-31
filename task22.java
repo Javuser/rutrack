ca4fe47f54b4c002bd7a6abfd5f15250a85f3c95
Javuser <agybetov.nurbakyt@gmail.com>
2017-03-31 12:31:20 +0600

    22 есеп

--- task22.java
+++ task22.java
@@ -2,17 +2,17 @@ package Anything;
 
 import java.util.Scanner;
 
-public class Main {
+public class task22 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         float a,b,c,A,h,s;
         a = scanner.nextInt();
         b = scanner.nextInt();
-        A = scanner.nextInt();
+        A = scanner.nextInt();//угол Альфа
         //Math.cos(A) = ((a-b)/2)/c;
         c = (float) ((float)(a-b)/2*Math.cos(A));
-        s = (float) ((float) c*Math.sin(A)*(a - (c * Math.cos(A))));
-        System.out.println(s);
+        s = (float) ((float) c*Math.sin(A)*(a - (c * Math.cos(A))));//Площадь трапеции
+        System.out.println(s);//Выводим площадь
     }
 }
