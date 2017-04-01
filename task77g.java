8581799c0c3642266fcf5879c44d72bdfdb5384e
Javuser <agybetov.nurbakyt@gmail.com>
2017-04-01 12:50:30 +0600

    77g esep

--- task77g.java
+++ task77g.java
@@ -2,7 +2,7 @@ package Anything;
 
 import java.util.Scanner;
 
-public class Main {
+public class task77g {
     // Задача про синусов
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
