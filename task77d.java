fde608a9c273ac885c60867798d3c98aef8b93ca
Javuser <agybetov.nurbakyt@gmail.com>
2017-04-01 13:21:20 +0600

    77d esep

---
+++ task77d.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double n,i,s,a;
+        s = 0;
+        System.out.print("Введите значение n=");
+        n = scanner.nextInt();
+        for(i=0;i<=n;i++){
+            s =Math.sqrt(2+s);//summ
+        }
+        System.out.println("Сумма равно:"+s);
+    }
+}
