562689781edaf3991421efc84e9648a965a82cb8
Javuser <agybetov.nurbakyt@gmail.com>
2017-04-01 12:40:08 +0600

    31 esep

---
+++ task31.java
@@ -0,0 +1,45 @@
+package Anything;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a;
+        a = scanner.nextInt();
+        System.out.println(" a)");
+        System.out.println(Math.pow(a,4));
+        System.out.println(" ");
+        System.out.println(" b)");
+        System.out.println(Math.pow(a,6));
+        System.out.println(" ");
+        System.out.println(" c)");
+        System.out.println(Math.pow(a,7));
+        System.out.println(" ");
+        System.out.println(" d)");
+        System.out.println(Math.pow(a,8));
+        System.out.println(" ");
+        System.out.println(" e)");
+        System.out.println(Math.pow(a,9));
+        System.out.println(" ");
+        System.out.println(" f)");
+        System.out.println(Math.pow(a,10));
+        System.out.println(" ");
+        System.out.println(" g)");
+        System.out.println(Math.pow(a,13));
+        System.out.println(" ");
+        System.out.println(" h)");
+        System.out.println(Math.pow(a,15));
+        System.out.println(" ");
+        System.out.println(" j)");
+        System.out.println(Math.pow(a,21));
+        System.out.println(" ");
+        System.out.println(" i)");
+        System.out.println(Math.pow(a,28));
+        System.out.println(" ");
+        System.out.println(" k)");
+        System.out.println(Math.pow(a,64));
+        System.out.println(" ");
+    }
+}
