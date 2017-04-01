698d03d17e842be86d26329b822360c1cefa3758
Javuser <agybetov.nurbakyt@gmail.com>
2017-04-01 12:47:55 +0600

    77 esep

---
+++ task77.java
@@ -0,0 +1,13 @@
+package Anything;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        n = scanner.nextInt();
+        System.out.println(Math.pow(2,n));
+    }
+}
